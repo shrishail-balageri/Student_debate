e27f47931c4ca89d36274d9cec56fc92038e1760
shrishail-balageri <shrishailbalageri@gmail.com>
2025-05-25 14:57:12 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,28 @@
+// main.dart
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import './login_screen.dart';
+//import './debate_list_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Debate App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginScreen(),
+    );
+  }
+}
+
+// screens/login_screen.dart
+
+
+// screens/debate_list_screen.dart
+
