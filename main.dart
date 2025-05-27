1b32c834881afed3dc9041aa015b2e17e2c701bb
shrishail-balageri <shrishailbalageri@gmail.com>
2025-05-27 22:23:17 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,66 +1,25 @@
 import 'package:flutter/material.dart';
-import 'debate_list_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'login_screen.dart';
 
-class SignUpScreen extends StatefulWidget {
-  @override
-  _SignUpScreenState createState() => _SignUpScreenState();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
-class _SignUpScreenState extends State<SignUpScreen> {
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
-
-  void _signup() async {
-    try {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: _emailController.text.trim(),
-        password: _passwordController.text.trim(),
-      );
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (_) => DebateListScreen()),
-      );
-    } catch (e) {
-      print(e);
-      ScaffoldMessenger.of(
-        context,
-      ).showSnackBar(SnackBar(content: Text('SignUp failed: ${e.toString()}')));
-    }
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: Text('SignUP'),
-        backgroundColor: Colors.black,
-        titleTextStyle: TextStyle(color: Colors.blue, fontSize: 24),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(labelText: 'Email'),
-              style: TextStyle(color: Colors.blue),
-            ),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(labelText: 'Password'),
-              style: TextStyle(color: Colors.blue),
-
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(onPressed: _signup, child: Text('SignUp')),
-          ],
-        ),
-      ),
-    );
-  }
+    return MaterialApp(
+      title: 'Debate App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginScreen(),);
+}
 }
 
+// screens/login_screen.dart
+
+
+// screens/debate_list_screen.dart
 
