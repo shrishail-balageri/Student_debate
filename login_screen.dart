a860575e3c48f11f5d3476f497d035418076dc72
shrishail-balageri <shrishailbalageri@gmail.com>
2025-05-27 22:27:57 +0530

    Add files via upload

--- login_screen.dart
+++ login_screen.dart
@@ -1,6 +1,7 @@
-// screens/login_screen.dart
 import 'package:flutter/material.dart';
+import 'debate_list_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import './signup_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -17,28 +18,67 @@ class _LoginScreenState extends State<LoginScreen> {
         email: _emailController.text.trim(),
         password: _passwordController.text.trim(),
       );
-      Navigator.pushReplacement(context,
-        MaterialPageRoute(builder: (_) => DebateListScreen()));
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => StudentDebateApp()),
+      );
     } catch (e) {
       print(e);
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Login failed')),
-      );
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Login failed: ${e.toString()}')));
     }
+  }
+
+  void _signup() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (_) => SignUpScreen()),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login')),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Login'),
+        backgroundColor: Colors.white,
+        titleTextStyle: TextStyle(color: Colors.green.shade700, fontSize: 30),
+      ),
       body: Padding(
         padding: EdgeInsets.all(16),
+
         child: Column(
           children: [
-            TextField(controller: _emailController, decoration: InputDecoration(labelText: 'Email')),
-            TextField(controller: _passwordController, decoration: InputDecoration(labelText: 'Password'), obscureText: true),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+              style: TextStyle(color: Colors.black),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+              style: TextStyle(color: Colors.black),
+            ),
             SizedBox(height: 20),
-            ElevatedButton(onPressed: _login, child: Text('Login')),
+            ElevatedButton(
+              onPressed: _login,
+              child: Text('Login'),
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.green.shade700,
+              ),
+            ),
+
+            TextButton(
+              child: Text('Create Account', style: TextStyle(fontSize: 15)),
+              onPressed: () => _signup(),
+              style: TextButton.styleFrom(
+                alignment: Alignment(13, 13),
+                foregroundColor: Colors.green.shade700,
+              ),
+            ),
           ],
         ),
       ),
